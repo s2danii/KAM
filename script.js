751db8507a520694b7aadf11419ae6b250dced61
Danii Shen <s2d.shen@gmail.com>
2019-10-03 14:58:21 -0400

    updated testimonials and services animation

--- script.js
+++ script.js
@@ -34,18 +34,70 @@ app.services = function () {
         $(`.serviceInfo`).css(`display`, `none`);
         $(`.${clickedDial}`).fadeToggle(function(){
             duration: 3000;
-        });
-
-        
-        
+        });                
     })
 }
 
+app.animateValue = function (id, start, end, duration) {
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let obj = document.getElementById(id);
+    let timer = setInterval(function () {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+
+}
+
+app.scrollPercent = function () {
+    let services = $(`#services`).offset().top + $(`#services`).outerHeight();
+    $(window).scroll(function () {
+        let scrollHeight = ($(window).scrollTop() + $(window).height())
+        if (scrollHeight >= services) {
+            app.animateValue('accValue', 0, 90, 2000);
+            app.animateValue('ipoValue', 0, 85, 2000);
+            app.animateValue('cfoValue', 0, 75, 2000);
+            $(window).off(`scroll`);
+        }
+    })
+}
+
+app.testimonials = function () {
+
+    $('.cd-testimonials-wrapper').flexslider({
+        //declare the slider items
+        selector: ".cd-testimonials > li",
+        animation: "slide",
+        //do not add navigation for paging control of each slide
+        controlNav: false,
+        slideshow: false,
+        //Allow height of the slider to animate smoothly in horizontal mode
+        smoothHeight: true,
+        start: function () {
+            $('.cd-testimonials').children('li').css({
+                'opacity': 1,
+                'position': 'relative'
+            });
+        }
+    });
+        
+
+}
+
+
 // INIT FUNCTION
 app.init = function () {
+    AOS.init();
     app.seeInfo();
     app.smoothScroll();
     app.services();
+    app.testimonials();
+    app.scrollPercent();
 };
 
 // DOCUMENT READY
